6475614502cee72ee30d6da6980cebac39ae42a2
Krishi Saripalli <krishi_saripalli@brown.edu>
2023-01-18 16:46:50 -0800

    proj section removed

--- src/App.jsx
+++ src/App.jsx
@@ -9,27 +9,26 @@ import ProjSection from './components/ProjSection';
 
 
 const App = () => (
-    <div className="  font-slab font-light text-white bg-gradient-to-r from-slate-800 to-blue-700">
-       <div className="sticky top-0  z-40">
-            <NavBar></NavBar>
-        </div>
-      <div id="fadeDiv" className=" three px-10 md:px-32 ">
-        <div className=" md:px-32">
-          <MidSection></MidSection>
-          <ProjSection></ProjSection>
-        </div>
-        <Footer ></Footer>
-      </div>
-        
-    
-      
-      
+  <div className="  font-slab font-light text-white bg-gradient-to-r from-slate-800 to-blue-700">
+    <div className="sticky top-0  z-40">
+      <NavBar></NavBar>
     </div>
-    
+    <div id="fadeDiv" className=" three px-10 md:px-32 ">
+      <div className=" md:px-32">
+        <MidSection></MidSection>
+      </div>
+      <Footer ></Footer>
+    </div>
 
-    
-  );
-  
+
+
+
+  </div>
+
+
+
+);
+
 
 
 export default App
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -5,41 +5,41 @@ function NavBar() {
     <nav class="backdrop-blur-md">
       <div class=" py-5  bg-transparent ">
         <div class="px-20">
-          
-          <div class="  flex items-center justify-evenly md:justify-between space-x-10 text-md md:text-xl">
-          <div className=''>
-            <a href="#top" className=' group md:mb-3 '>
-              <span class="material-symbols-outlined text-md md:text-4xl">forest</span>
-              <span class="block max-w-0 group-hover:max-w-full transition-all duration-700 h-0.5 bg-white "></span>
-            </a>        
-          </div>
-          <div className="inline-flex gap-10">
-            <div>
+
+          <div class="  flex items-center justify-evenly lg:justify-between space-x-10 text-md md:text-xl">
+            <div className=''>
+              <a href="#top" className=' group md:mb-3 '>
+                <span class="material-symbols-outlined text-md md:text-4xl">forest</span>
+                <span class="block max-w-0 group-hover:max-w-full transition-all duration-700 h-0.5 bg-white "></span>
+              </a>
+            </div>
+            <div className="inline-flex gap-10">
+              <div>
                 <a href="https://github.com/krishi-saripalli" className=' group md:mb-3 '>
                   <span class="">GitHub</span>
                   <span class="block max-w-0 group-hover:max-w-full transition-all duration-700 h-0.5 bg-white "></span>
-                </a>        
+                </a>
               </div>
               <div>
                 <a href="https://www.linkedin.com/in/krishi-saripalli-8b48a4142/" className=' group md:mb-3 '>
                   <span class="">LinkedIn</span>
                   <span class="block max-w-0 group-hover:max-w-full transition-all duration-700 h-0.5 bg-white "></span>
-                </a>        
+                </a>
               </div>
               <div>
                 <a href="https://twitter.com/krishi_sa" className=' group md:mb-3 '>
                   <span class="">Twitter</span>
                   <span class="block max-w-0 group-hover:max-w-full transition-all duration-700 h-0.5 bg-white "></span>
-                </a>        
+                </a>
               </div>
-          </div>
-            
+            </div>
+
           </div>
         </div>
       </div>
     </nav>
-    
-    
+
+
   )
 }
 
